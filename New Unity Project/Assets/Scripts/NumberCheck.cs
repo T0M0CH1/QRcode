994a34f68fb15cb59f07687d56efbc3038157001
岩崎友哉 <tomoti2525@yahoo.co.jp>
2019-06-29 21:12:24 +0900

    First Commit

---
+++ New Unity Project/Assets/Scripts/NumberCheck.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
+using System;
+
+public class NumberCheck : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+    public static InputField inputField;
+    private int stringLength;
+    private int stringByte;
+    private string modeJudge = "";
+    private string binary = "";
+
+    void Start()
+    {
+        inputField = this.GetComponent<InputField>();
+        inputField.ActivateInputField();
+    }
+   
+    public void checkZenkaku()
+    {
+        if (inputField.text != null)
+        {
+            //入力した文字数を計算
+            stringLength = inputField.text.Length;
+            //Byteの数を計算
+            stringByte = Encoding.GetEncoding("Shift_JIS").GetByteCount(inputField.text);
+            StringJudge();  
+            //文字数とByte数を比べ、全角が入っていたら弾く処理
+            if (stringLength == stringByte)
+            {                
+                text.text = "QRコード作成";
+                Debug.Log(modeJudge);
+                //入力された文字数を10進数から2進数にする
+                ConvertBinary(stringLength);
+            }
+            else
+            {
+                text.text = "入力された文字が全角です。すべて半角で入力してください。";
+                stringLength = 0;
+                stringByte = 0;
+                inputField.ActivateInputField();
+            }
+        }
+        else
+        {
+            text.text = "文字が入力されていません。";
+        }
+        inputField.text = "";
+        inputField.ActivateInputField();
+    }
+
+    private void StringJudge()
+    { 
+        //InputFieldに打った文字分回す
+        for(int i = 1; i < stringLength; i++)
+        {
+            //英数字か数字だけかを判定する処理
+            if (Char.IsDigit(inputField.text, i) == false)
+            {            
+                //英数字の場合
+                modeJudge = "0010";                
+            }
+            else
+            {
+                //数字のみの場合
+                modeJudge = "0001";
+            }
+        }
+    }
+
+    private void ConvertBinary(int decimalNum)
+    {
+        //2進数に変換する処理
+        binary = System.Convert.ToString(decimalNum, 2);
+
+        //英数字の場合は9bitで表す(余りは0詰め)       
+        if (modeJudge == "0010")
+        {
+            binary.PadLeft(9, '0');
+        }
+        //数字のみの場合は10bitで表す(余りは0詰め)
+        if (modeJudge == "0001")
+        {
+            binary.PadLeft(10, '0');
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/ToBinary.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ToBinary : MonoBehaviour
+{
+    private int stringDate = 0;
+    private int twoCut = 0;
+    private char textStr = ' ';
+    private string binaryString = "";
+    private int remainder = 0;
+    private string remainStr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //InputFieldのテキストの長さが奇数だった場合入る。
+        if (NumberCheck.inputField.text.Length % 2 == 1)
+        {
+            //最後の文字を先に2進数にして取っておく
+            remainder = StringToBinary(NumberCheck.inputField.text[NumberCheck.inputField.text.Length]);            
+            remainStr = remainder.ToString();
+            //奇数の最後の文字は6bitで表す(余りは0詰め)
+            remainStr.PadLeft(6, '0');           
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //InputFieldのテキストの長さ分回す
+        for(int i = 0; i < NumberCheck.inputField.text.Length; i++)
+        {
+            //一文字ずつ2進数化していく
+            textStr = NumberCheck.inputField.text[i];            
+            //仮想的に2文字ずつに分け、一文字目は45をかける。二文字目は一文字目の数字に足す。
+            CalString(i, StringToBinary(textStr));
+        }
+    }
+
+    public int StringToBinary(char str)
+    {
+        switch (str)
+        {
+            case '0':
+                stringDate = 0;
+                return stringDate;
+        }
+        return -1;
+    }   
+
+    private void CalString(int i, int strDate)
+    {
+        if (i % 2 == 1)
+        {
+            //もし奇数が最後の文字だった場合入る
+            if(i == NumberCheck.inputField.text.Length)
+            {
+                binaryString += remainStr;
+                return;
+            }            
+            twoCut = stringDate * 45;
+        }
+        else if (i % 2 == 0)
+        {
+            twoCut += stringDate;
+            //
+            binaryString += twoCut.ToString();
+            twoCut = 0;            
+        }
+    }
+}
